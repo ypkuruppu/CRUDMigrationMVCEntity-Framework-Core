610d18f54283687e4d9f94900d6de3d572aef62e
HP <HP@DESKTOP-Q6LFOL4>
2025-05-01 16:05:20 +0530

    Add project files.

---
+++ CRUDMigrationMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CRUDMigrationMVC.Models;
+
+namespace CRUDMigrationMVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> employees { get; set; }
+
+    }
+}
---
+++ CRUDMigrationMVC/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace CRUDMigrationMVC.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
